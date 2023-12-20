af2b24d4ebfad933f65175ea6c8c86d9b118f900
alvaro.obregon <alvaro911>
2023-12-19 21:21:29 -0500

    20231219

--- WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/manejadores/ManejadorArbol.java
+++ WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/manejadores/ManejadorArbol.java
@@ -1,9 +1,46 @@
 package com.grupo.ii.poo.manejadores;
 
+import com.grupo.ii.poo.recursos.Nodo;
+
 public class ManejadorArbol {
 
+	private Nodo inicial;
+	
+	
 	
 	public void agregarDato(int dato) {
 		
+		if(inicial == null) {
+			
+			inicial = new Nodo();
+			inicial.setNumero(dato);
+			
+		} else {
+			Nodo temp = inicial;
+			Nodo nuevo = new Nodo();
+			nuevo.setNumero(dato);
+			while(temp != null) {
+				
+				if(temp.getNumero() > dato) {
+					//derecha
+					if(temp.getMayor() == null) {
+						temp.setMayor(nuevo);
+					} else {
+						
+					}
+				} else {
+					//izquierda
+					if(temp.getMenor() == null) {
+						temp.setMenor(nuevo);
+					} else {
+						
+					}
+					
+				}				
+			}
+			
+			System.out.println("aqui continuaria");
+			
+		}
 	}
 }
