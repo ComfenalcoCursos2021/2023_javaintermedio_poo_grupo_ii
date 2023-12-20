49194a5febbca5e483271b249a82a1755cee9502
alvaro.obregon <alvaro911>
2023-12-19 19:37:04 -0500

    20231219

--- WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/manejadores/ManejadorArbol.java
+++ WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/manejadores/ManejadorArbol.java
@@ -2,4 +2,8 @@ package com.grupo.ii.poo.manejadores;
 
 public class ManejadorArbol {
 
+	
+	public void agregarDato(int dato) {
+		
+	}
 }
--- WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/recursos/Nodo.java
+++ WORKSPACE/ArborBinarioV1/src/com/grupo/ii/poo/recursos/Nodo.java
@@ -7,5 +7,24 @@ public class Nodo {
 	private Nodo menor;
 	private Nodo mayor;
 	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public Nodo getMenor() {
+		return menor;
+	}
+	public void setMenor(Nodo menor) {
+		this.menor = menor;
+	}
+	public Nodo getMayor() {
+		return mayor;
+	}
+	public void setMayor(Nodo mayor) {
+		this.mayor = mayor;
+	}
+	
 	
 }
